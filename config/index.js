77ebf40aa5c4839af5860334fe44554027c68071
Nedbie Francis Amoranto <hindakosned@gmail.com>
2019-05-28 15:38:52 +0800

    Remove mandatory usage of db url if present in .env

--- config/index.js
+++ config/index.js
@@ -17,10 +17,6 @@ const config = Object.assign({
 module.exports = config;
 
 function loadDbConfig() {
-  if(process.env.DATABASE_URL) {
-    return process.env.DATABASE_URL;
-  }
-
   if(fs.existsSync(path.join(__dirname, './database.js'))) {
     return require('./database')[ENV];
   }
